aacac7b85c062dabde2a2eedea6552035ab29668
shovoncse <shovondas26@gmail.com>
2024-01-25 23:55:42 +0200

    :white_check_mark: solved

--- 704. Binary Search/solution.py
+++ 704. Binary Search/solution.py
@@ -16,3 +16,20 @@ class Solution:
                 return rec(nums, target, mid + 1, end)
         
         return rec(nums, target, 0, len(nums) - 1)
+
+"""
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                return m
+        return -1
+"""
